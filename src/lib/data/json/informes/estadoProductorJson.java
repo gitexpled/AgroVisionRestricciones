159c991d64003498974da0175d73fb7d4f28f7e7
gitexpled <49001587+gitexpled@users.noreply.github.com>
2024-09-17 08:49:51 -0300

    mejora en los informes

--- src/lib/data/json/informes/estadoProductorJson.java
+++ src/lib/data/json/informes/estadoProductorJson.java
@@ -37,6 +37,7 @@ import lib.db.bloqueadoDB;
 import lib.db.bloqueadoOpDB;
 import lib.db.especieDB;
 import lib.db.estadoProductorDB;
+import lib.db.estadoProductorNewDB;
 import lib.db.informesDB;
 import lib.db.mailDB;
 import lib.sap.Sync;
@@ -61,11 +62,11 @@ public class estadoProductorJson {
 
 	
 	
-	@RequestMapping(value = "/detalleRest/{mercado}/{especie}/{productor}/{parcela}/{turno}/{variedad}", method = { RequestMethod.GET })
+	@RequestMapping(value = "/detalleRest/{mercado}/{especie}/{productor}/{etapa}/{campo}/{turno}/{variedad}", method = { RequestMethod.GET })
 	public void getData(HttpServletResponse response, @PathVariable("mercado") String mercado,
 			@PathVariable("especie") String especie, @PathVariable("productor") String productor,
-			@PathVariable("parcela") String parcela, @PathVariable("turno") String turno,
-			@PathVariable("variedad") String variedad, HttpSession httpSession)
+			@PathVariable("etapa") String etapa, @PathVariable("campo") String campo,
+			@PathVariable("turno") String turno,@PathVariable("variedad") String variedad, HttpSession httpSession)
 			throws Exception {
 		
 		session ses = new session(httpSession);
@@ -96,7 +97,8 @@ public class estadoProductorJson {
 		
 		
 		
-		html+="<tr><td width='120px'>Parcela</td><td> "+parcela+"</td></tr>";
+		html+="<tr><td width='120px'>Etapa</td><td> "+etapa+"</td></tr>";
+		html+="<tr><td>Campo</td><td> "+campo+"</td></tr>";
 		html+="<tr><td>Turno</td><td> "+turno+"</td></tr>";
 		html+="<tr><td>Variedad</td><td> "+variedad+"</td></tr>";
 		html+="</table>";
@@ -139,8 +141,8 @@ public class estadoProductorJson {
 		
 		String exporta="Y";
 		
-		
-		exporta=estadoProductorDB.getEstadoProductor(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),mercado,productor,parcela,turno);
+		estadoProductorNewDB dataDB=new estadoProductorNewDB();
+		exporta=dataDB.getEstadoProductor(ses.getIdTemporada(),espe.getIdEspecie(),variedad.trim(),mercado,productor,etapa,campo,turno);
 		if (exporta.equals("SI"))
 			html+="<tr><td>Habilitado</td><td bgcolor='green' align='center'>SI</td></tr>";
 		else
@@ -153,26 +155,25 @@ public class estadoProductorJson {
 		html+="</td></tr></table><br><br><br>";
 		
 		if (!rPorcentaje.equals("No"))
-			html+=estadoProductorDB.getBlockPorcentaje(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,parcela,turno);
+			html+=estadoProductorDB.getBlockPorcentaje(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,etapa,turno);
 			html+="<br>";
 		if (!rPorcentajeArfD.equals("No"))
-			html+=estadoProductorDB.getBlockPorcentajeArfD(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,parcela,turno);
+			html+=estadoProductorDB.getBlockPorcentajeArfD(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,etapa,turno);
 			html+="<br>";
 		
 		if (!rMoleculas.equals("No"))
-		html+=estadoProductorDB.getBlockMolecula(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,parcela,turno);
+		html+=estadoProductorDB.getBlockMolecula(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,etapa,turno);
 		html+="<br>";
 		
 		if (!rProductor.equals("No"))
-			html+=estadoProductorDB.getBlockProductor(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,parcela,turno);
+			html+=estadoProductorDB.getBlockProductor(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"",productor,etapa,turno);
 		
-
+		informesDB info=new informesDB();
 		
-		html += "<div style='width:70%; text-align: center;'><b  style='font-size:18px'>Resultado de pesticidas</b></div>";
-		html += "<b  style='font-size:18px'>Carga Automatica</b>";
-		html+=informesDB.getDetalleRestriccion(espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"", productor,parcela,turno);
-		html += "<br/><b  style='font-size:18px'>Carga Manual</b>";
-		html+=informesDB.getDetalleRestriccionM(espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"", productor,parcela,turno);
+		html += "<div style='width:70%; text-align: center;'><b  style='font-size:18px'>Resultado de pesticidas("+ses.getIdTemporada()+")</b></div>";
+		//html += "<b  style='font-size:18px'>Carga Automatica</b>";
+		html+=info.getDetalleRestriccion(ses.getIdTemporada(),espe.getIdEspecie(),(variedad.trim()),m.getIdMercado()+"", productor,etapa,campo,turno);
+		
 		html+="";
 	OutputStream outputStream2;
 	try {
@@ -338,23 +339,23 @@ public class estadoProductorJson {
 		data.setDraw(0);
 		session ses = new session(httpSession);
 		
-		
 		System.out.println("GET:::::::::::::::::::::::::::::::::::::::: ");
 		Map<String, String[]> parameters = request.getParameterMap();
 		ArrayList<filterSql> filter = new ArrayList<filterSql>();
-		int idEspecie=1;
+		int idEspecie=7;
 		String productor = "";
 		
-		String parcela = "";
+		String etapa = "";
+		String campo = "";
 		String turno = "";
 		String variedad = "";
 		System.out.println("------------- ----------------- --------------- -------------- ------------------");
 		for (String key : parameters.keySet()) {
-			System.out.println(key);
+			//System.out.println(key);
 			if (key.startsWith("vw_")) {
 				String[] vals = parameters.get(key);
 				for (String val : vals) {
-					System.out.println(key+" -> " + val);
+				//	System.out.println(key+" -> " + val);
 					filterSql fil = new filterSql();
 					fil.setCampo(key.substring(3));
 					fil.setValue(val);
@@ -374,10 +375,17 @@ public class estadoProductorJson {
 						}catch(Exception e)
 						{}
 					}
-					if (key.equals("vw_parcela"))
+					if (key.equals("vw_etapa"))
 					{
 						try{
-							parcela=val;
+							etapa=val;
+						}catch(Exception e)
+						{}
+					}
+					if (key.equals("vw_campo"))
+					{
+						try{
+							campo=val;
 						}catch(Exception e)
 						{}
 					}
@@ -398,15 +406,17 @@ public class estadoProductorJson {
 				}
 			}
 		}
+		
 		System.out.println("ses.isValid()"+ses.isValid());
 		
 		if (!ses.isValid()) {
 			
 			data.setDraw(0);
 			data.init();
+			estadoProductorNewDB estadoP=new estadoProductorNewDB();
 			try {
-				System.out.println("idEspecie: "+idEspecie+"---productor: "+productor);
-				ArrayList<String[]> pp=estadoProductorDB.getEstadoProductorA(ses.getIdTemporada(),idEspecie,productor,parcela,turno,variedad,false,"");
+				System.out.println("idEspecie: "+idEspecie+", etapa: "+etapa+", campo: "+campo+", turno: "+turno+"---productor: "+productor);
+				ArrayList<String[]> pp=estadoP.getEstadoProductorA(ses.getIdTemporada(),idEspecie,productor,etapa,campo,turno,variedad,false,"");
 				data.setDatas(pp);
 				data.setRecordsFiltered(pp.size());
 				data.setRecordsTotal(pp.size());
@@ -416,6 +426,8 @@ public class estadoProductorJson {
 			}
 			
 		}
+	
+		
 		return data;
 		
 
--- src/lib/db/informesDB.java
+++ src/lib/db/informesDB.java
@@ -8,7 +8,93 @@ import java.util.ArrayList;
 
 public class informesDB {
 	
-	public static String getDetalleRestriccion(int idEspecie,String idVariedad, String mercado,String productor,String parcela,String turno) throws Exception {
+	public ArrayList<ArrayList<String>> getLmrDetalle(int idTemporada,int idEspecie,String idVariedad, String Mercado,String productor,String etapa,String campo,String turno) throws Exception {
+		ArrayList<ArrayList<String>> list = new ArrayList<>();
+
+		ConnectionDB db = new ConnectionDB();
+		Statement stmt = null;
+		String sql = "";
+		try {
+
+			stmt = db.conn.createStatement();
+
+			sql = " SELECT r.creado,r.entregado,r.code,  IF(r.producto LIKE '%fosetyl%','fosetil',IF(r.producto LIKE '%fosetil%','fosetil',r.producto)) AS codProducto, '' codTurno,  ";
+			sql += " r.lmr as resultado,l.limite lmr,l.limite2 lmr2,CONCAT(format((( r.lmr * 100) / IFNULL(l.limite2, l.limite)),2),'%'),'' as vigente,IF(r.lmr<l.limite or r.lmr=0 , 0, 1) as habilitado,1 as id ";
+			sql += " FROM jerarquias j JOIN temporada t ";
+			sql += " inner join  especie e on (j.Especie=e.pf)  ";
+			sql += " inner join  resultadoDet r on (r.productor=j.Productor and r.etapa=j.Etapa and r.campo=j.Campo and r.turno=j.Turno and r.especie=e.codLab   and r.variedad=j.VariedadDenomina)  ";
+			sql += " inner join diccionario d on (d.codRemplazo=r.producto) ";
+			sql += " inner join vw_limite l on (d.codProducto=l.codProducto and e.idEspecie=l.idEspecie)   ";
+			sql += " inner join vw_mercados m on (l.idMercado=m.idMercado)  ";
+			sql += "where  ";
+			sql += " t.idTemporada='"+idTemporada+"'  ";
+			
+			sql += "  and e.idEspecie='"+idEspecie+"' ";
+			
+			if (!productor.isEmpty())
+			sql += " and j.Productor='"+productor+"' ";
+			
+			if (!etapa.isEmpty())
+				sql += " and j.Etapa='"+etapa+"' ";
+			
+			if (!campo.isEmpty())
+				sql += " and j.Campo='"+campo+"' ";
+				
+			if (!turno.isEmpty())
+				sql += " and j.Turno='"+turno+"' ";
+			
+			if (!idVariedad.isEmpty())
+				sql += " and j.VariedadDenomina='"+idVariedad+"' ";
+			if (!Mercado.isEmpty())
+				sql += " and m.idMercado='"+Mercado+"' ";
+		
+			//sql+="Order by Idmailexcel desc";
+
+			System.out.println(sql);
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				ArrayList<String> l = new ArrayList<>();
+				l.add(rs.getString(1));
+				l.add(rs.getString(2));
+				l.add(rs.getString(3));
+				l.add(rs.getString(4));
+				l.add(rs.getString(5));
+				l.add(rs.getString(6));
+				l.add(rs.getString(7));
+				l.add(rs.getString(8));
+				l.add(rs.getString(9));
+				
+				if (rs.getString(10).equals("1"))
+					l.add("SI");
+				else
+					l.add("NO");
+				if (rs.getString(11).equals("0"))
+					l.add("SI");
+				else
+					l.add("NO");
+				
+				l.add(rs.getString(12));
+				
+
+				list.add(l);
+			}
+			rs.close();
+			stmt.close();
+			db.close();
+			
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error: " + e.getMessage());
+			System.out.println("sql: " + sql);
+			throw new Exception("getLmr: " + e.getMessage());
+		} finally {
+			db.close();
+		}
+
+		return list;
+	}
+	public  String getDetalleRestriccion(int idtemporada,int idEspecie,String idVariedad, String mercado,String productor,String etapa,String campo,String turno) throws Exception {
 		ArrayList<String[]> data = new ArrayList<>();
 
 		ConnectionDB db = new ConnectionDB();
@@ -17,59 +103,28 @@ public class informesDB {
 		String html="";
 		try {
 
-			 stmt = db.conn.createStatement();
-
-			 sql = "{CALL sp_viewDetalleRest("+idEspecie+",'"+idVariedad+"','"+mercado+"','"+productor+"','"+parcela+"','"+turno+"') }";
-			 System.out.println(sql);
-			// ResultSet rs = stmt.executeQuery(sql);
-			ResultSet rs = stmt.executeQuery(sql);
-
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnCount = rsmd.getColumnCount();
-
-			int j=0;
-			html="<table border=1>";
 			
-			html+="<tr>";
-			for (int i = 1; i <= columnCount; i++) {
-				html+="<td>&nbsp;";
-			
-				if (rsmd.getColumnLabel(i).equals("resultado"))
-					html+="Resultado (ppm)";
-				else
-				{
-					String htmlData  = rsmd.getColumnLabel(i); 
-					htmlData = htmlData.substring(0,1).toUpperCase() + htmlData.substring(1).toLowerCase();
-					html+=htmlData;
-				}
-				html+="&nbsp;</td>";
-				
-			}
-			html+="</tr>";
-			
-			while (rs.next()) {
 
-				String[] o = new String[columnCount];
+			 ArrayList<ArrayList<String>> dataLmr=getLmrDetalle(idtemporada, idEspecie, idVariedad, mercado, productor, etapa,campo, turno);
+			 
+			 html="<table border=1>";
+			for (ArrayList<String> arrayList : dataLmr) {
 				html+="<tr>";
-				for (int i = 1; i <= columnCount; i++) {
+				for (String arr : arrayList) {
 					html+="<td>&nbsp;";
-					o[i-1] = rs.getString(i);
-					if (rs.getString(i)==null)
+					
+					if (arr==null)
 						html+="";
 					else
-						html+=o[i-1];
+						html+=arr;
 					html+="&nbsp;</td>";
-					
 				}
 				html+="</tr>";
-				++j;
-				data.add(o);
 			}
 			 html+="</table>";
-			rs.close();
 			
-			stmt.close();
-			db.conn.close();
+			
+		
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -77,7 +132,7 @@ public class informesDB {
 			System.out.println("sql: " + sql);
 			throw new Exception("getEstadoProductor: " + e.getMessage());
 		} finally {
-			db.close();
+			
 		}
 
 		return html;
